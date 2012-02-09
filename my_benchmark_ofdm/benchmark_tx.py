6a611fde681a0436a8e3fea47877ae255bf8cd35
UpYou <lzyou@smail.nju.edu.cn>
2012-02-09 23:13:25 +0800

    Adding write_complex_binary.m to write samples into file

--- my_benchmark_ofdm/benchmark_tx.py
+++ my_benchmark_ofdm/benchmark_tx.py
@@ -111,6 +111,7 @@ def main():
         if options.discontinuous and pktno % 5 == 4:
             time.sleep(1)
         pktno += 1
+	time.sleep(10)
         
     send_pkt(eof=True)
     tb.wait()                       # wait for it to finish
